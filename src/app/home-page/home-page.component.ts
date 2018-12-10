ff5aed9ea4650953da8a0fe52a7c8a6d09a67107
Roman Gorelik <romang31@gmail.com>
2018-12-10 18:50:25 -0500

    Added functionality to the home page, and added a logout button. Will continue with making the checking and savings pages

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import firebase from '../helpers/firebase.js'
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  checkings: number = 0
+  savings: number = 0
+  total: number = this.checkings + this.savings
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToChecking(): void {
+    this.router.navigateByUrl('/checking')
+  }
+
+  goToSavings(): void {
+    this.router.navigateByUrl('/saving')
+  }
+
+  logOut() {
+    return firebase
+      .auth()
+      .signOut()
+      .then((response) => {
+        this.router.navigateByUrl('/login')
+      });
   }
 
 }
